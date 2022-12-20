74f0de91a14859cbe89a6419341fb2e9ef7a818f
belhub <belhub24@gmail.com>
2022-12-20 13:18:11 +0100

    usuwanie
    bledy z mergem byly to usuwam i wrzucam od nowa

--- beer-timer/beer-timer/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace beer_timer.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
