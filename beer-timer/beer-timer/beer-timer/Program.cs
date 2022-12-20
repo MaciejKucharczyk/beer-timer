1bb6fa10f9e2b7998f89906df222ee2ccdec64cc
belhub <belhub24@gmail.com>
2022-12-20 10:52:51 +0100

    zduplikowało mi wszystko
    czy teraz też zduplikuje? kto wie

---
+++ beer-timer/beer-timer/beer-timer/Program.cs
@@ -0,0 +1,56 @@
+using beer_timer.Data;
+using beer_timer.Interfaces;
+using beer_timer.Models;
+using beer_timer.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddTransient<IRankingService, RankingService>();
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddControllersWithViews();
+
+//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(connectionString));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
---
+++ beer-timer/beer-timer/beer-timer/Services/RankingService.cs
@@ -0,0 +1,55 @@
+﻿using beer_timer.Interfaces;
+using beer_timer.Models;
+
+namespace beer_timer.Services
+{
+    public class RankingService : IRankingService
+    {
+        private readonly DataContext _dataContext;
+
+        public RankingService(DataContext dataContext)
+        {
+            this._dataContext = dataContext;    
+        }
+
+        public int CreateRanking(Ranking ranking)
+        {
+            _dataContext.Rankings.Add(ranking);
+            _dataContext.SaveChanges();
+            return ranking.Id;
+           
+        }
+
+        public bool DeleteRanking(int id)
+        {
+            var rankingToDelete = _dataContext.Rankings.Find(id);
+            if (rankingToDelete == null) {
+                return false;
+            }
+            _dataContext.Rankings.Remove(rankingToDelete);
+            _dataContext.SaveChanges();
+            return true;
+        }
+
+        public Ranking GetRankingById(int id)
+        {
+            var drinks = _dataContext.Rankings.Find(id);
+            return drinks;
+        }
+
+        public List<Ranking> GetRankingList()
+        {
+            var rankings = _dataContext.Rankings.ToList();
+            return rankings;
+        }
+
+        public bool UpdateRanking(int id, Ranking ranking)
+        {
+            _dataContext.Rankings.Find(id).Sec=ranking.Sec;
+            _dataContext.Rankings.Find(id).Tens=ranking.Tens;
+            _dataContext.Rankings.Find(id).Technique=ranking.Technique;
+            _dataContext.Rankings.Find(id).UserNameId=ranking.UserNameId;
+            return true;
+        }
+    }
+}
